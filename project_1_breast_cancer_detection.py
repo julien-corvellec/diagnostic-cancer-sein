2621cd582aecc9d084db47c8baa7493f796dc1da
julien-corvellec <127233639+julien-corvellec@users.noreply.github.com>
2023-03-07 18:43:28 +0100

    Update project_1_breast_cancer_detection.py

--- project_1_breast_cancer_detection.py
+++ project_1_breast_cancer_detection.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dataset = pd.read_csv('/content/data.csv')
+dataset = pd.read_csv('data.csv')
 
 dataset = dataset.drop(columns='Unnamed: 32')
 
