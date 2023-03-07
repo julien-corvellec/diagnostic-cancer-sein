ed0b91949ccb567042679bc5e321564b9cbadc72
julien-corvellec <127233639+julien-corvellec@users.noreply.github.com>
2023-03-07 18:42:45 +0100

    Add files via upload

---
+++ project_1_breast_cancer_detection.py
@@ -0,0 +1,211 @@
+# Part 1: Data preprocessing
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read_csv('/content/data.csv')
+
+dataset = dataset.drop(columns='Unnamed: 32')
+
+if (dataset.isnull().values.any())
+    return "Error, null values"
+
+dataset = pd.get_dummies(data=dataset, drop_first=True)
+
+sns.countplot(dataset['diagnosis_M'], label='Count')
+plt.show()
+
+# benign (B) values
+(dataset.diagnosis_M == 0).sum()
+
+# malignant (M) values
+(dataset.diagnosis_M == 1).sum()
+
+dataset_2 = dataset.drop(columns='diagnosis_M')
+
+dataset_2.corrwith(dataset['diagnosis_M']).plot.bar(
+    figsize=(16,9), title = 'Correlation with diagnosis_M', 
+    rot = 45, grid = True
+)
+
+corr = dataset.corr()
+
+plt.figure(figsize=(20,10))
+sns.heatmap(corr, annot=True)
+
+"""## Splitting the dataset into train and test set"""
+
+# matrix of features / independent variables
+x = dataset.iloc[:, 1:-1].values
+
+# dependent variable
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+x_train
+
+x_test
+
+"""# Part 2: Building the model
+
+## 1) Logistic regression
+"""
+
+from sklearn.linear_model import LogisticRegression
+classifier_lr = LogisticRegression(random_state=0)
+classifier_lr.fit(x_train, y_train)
+
+y_pred = classifier_lr.predict(x_test)
+
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
+
+acc = accuracy_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+
+
+results = pd.DataFrame([['Logistic Regression', acc, f1, prec, rec]],
+               columns = ['Model', 'Accuracy', 'F1 Score', 'Precision', 'Recall'])
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+results
+
+"""### Cross validation"""
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier_lr, X=x_train, y=y_train, cv=10)
+
+print("Accuracy is {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation is {:.2f} %".format(accuracies.std()*100))
+
+"""## 2) Random forest"""
+
+from sklearn.ensemble import RandomForestClassifier
+classifier_rf = RandomForestClassifier(random_state=0)
+classifier_rf.fit(x_train, y_train)
+
+y_pred = classifier_rf.predict(x_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['Random Forest', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+
+results = results.append(model_results, ignore_index = True)
+results
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+"""### Cross validation"""
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier_rf, X=x_train, y=y_train, cv=10)
+
+print("Accuracy is {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation is {:.2f} %".format(accuracies.std()*100))
+
+"""## 3) XGBoost"""
+
+from xgboost import XGBClassifier
+classifier_xgb = XGBClassifier(random_state=0)
+classifier_xgb.fit(x_train, y_train)
+
+y_pred = classifier_xgb.predict(x_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['XGBoost', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+
+results = results.append(model_results, ignore_index = True)
+results
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+"""### Cross validation"""
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier_xgb, X=x_train, y=y_train, cv=10)
+
+print("Accuracy is {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation is {:.2f} %".format(accuracies.std()*100))
+
+"""# Part 3: Randomized Search to find the best parameters (Logistic regression)"""
+
+from sklearn.model_selection import RandomizedSearchCV
+
+parameters = [
+              {'penalty':['l1', 'l2', 'elasticnet', 'none'],
+               'C':[0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
+               'solver':['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+               }
+              ]
+
+parameters
+
+randomized_search = RandomizedSearchCV(estimator = classifier_lr, param_distributions = parameters,
+                                 n_iter = 5, scoring='roc_auc', n_jobs = -1, cv = 5, verbose=3)
+
+# cv: cross-validation
+# n_jobs = -1:
+# Number of jobs to run in parallel. -1 means using all processors
+
+randomized_search.fit(x_train, y_train)
+
+randomized_search.best_estimator_
+
+randomized_search.best_params_
+
+randomized_search.best_score_
+
+"""# Part 4: Final Model (Logistic regression)"""
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(C=0.5, class_weight=None, dual=False, fit_intercept=True,
+                   intercept_scaling=1, l1_ratio=None, max_iter=100,
+                   multi_class='auto', n_jobs=None, penalty='l2',
+                   random_state=0, solver='lbfgs', tol=0.0001, verbose=0,
+                   warm_start=False)
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['Final Logistic regression', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+
+results = results.append(model_results, ignore_index = True)
+results
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier, X=x_train, y=y_train, cv=10)
+
+print("Accuracy is {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation is {:.2f} %".format(accuracies.std()*100))
